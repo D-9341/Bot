a742d77c611ec3d84bfcbd57e8a82a478f3a3cfc
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-04-06 00:39:20 +0300

    Update code.py

--- code.py
+++ code.py
@@ -8,6 +8,42 @@ from discord.utils import get
 client = commands.Bot(command_prefix = "cephalon/")
 #like cephalon/support
 
+#система уровней
+@client.event
+async def on_message(message):
+    if message.author.bot == False:
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+ 
+ 
+        await update_data(users, message.author)
+        await add_experience(users, message.author, 5)
+        await level_up(users, message.author, message)
+   
+   
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+ 
+
+async def update_data(users, user):
+    if not f'{user.id}' in users:
+        users[f'{user.id}'] = {}
+        users[f'{user.id}']['experience'] = 0
+        users[f'{user.id}']['level'] = 1
+
+async def add_experience(users, user, exp):
+    users[f'{user.id}']['experience'] += exp
+
+
+async def level_up(users, user, message):
+    experience = users[f'{user.id}']['experience']
+    lvl_start = users[f'{user.id}']['level']
+    lvl_end = int(experience ** (1/4))
+    if lvl_start < lvl_end:
+        await message.channel.send(f'{user.mention} повысил свой уровень до {lvl_end}')
+        users[f'{user.id}']['level'] = lvl_end
+
+
 @client.command(pass_context = True)
 async def general(ctx, amount = 1):
     await ctx.channel.purge(limit = amount)
