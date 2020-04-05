f53b6fc44649c0354a693b0d405d608122fcf860
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-04-06 00:26:38 +0300

    Update code.py

--- code.py
+++ code.py
@@ -8,6 +8,10 @@ from discord.utils import get
 client = commands.Bot(command_prefix = "cephalon/")
 #like cephalon/support
 
-@client.event
+@client.command()
 async def ping(ctx):
     await ctx.send(f'pong!')
+
+token = 'Njk0MTcwMjgxMjcwMzEyOTkx.XopILA.BOCOCF8XSeqsgHtzzZOE0wSB1yQ'
+
+client.run(token)
