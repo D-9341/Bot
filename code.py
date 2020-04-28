6490ae751210bb9b9e149be055c1610e5ec37b37
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-04-28 20:32:26 +0300

    Update code.py

--- code.py
+++ code.py
@@ -10,40 +10,6 @@ from discord.utils import get
 client = commands.Bot(command_prefix = "cephalon/")
 #like cephalon/support
 
-@client.event
-async def on_message(message):
-    with open('users.json', 'r') as f:
-            users = json.load(f)
- 
- 
-    await update_data(users, message.author)
-    await add_experience(users, message.author, 5)
-    await level_up(users, message.author, message)
-   
-   
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-
-async def update_data(users, user):
-    if not f'{user.id}' in users:
-        users[f'{user.id}'] = {}
-        users[f'{user.id}']['experience'] = 0
-        users[f'{user.id}']['level'] = 1
- 
-
-async def add_experience(users, user, exp):
-    users[f'{user.id}']['experience'] += exp
-    
-    
-
-async def level_up(users, user, message):
-    experience = users[f'{user.id}']['experience']
-    lvl_start = users[f'{user.id}']['level']
-    lvl_end = int(experience ** (1/4))
-    if lvl_start < lvl_end:
-        await message.channel.send(f'{user.mention} достигнул {lvl_end} уровня!')
-        users[f'{user.id}']['level'] = lvl_end            
-            
 #проверка подключения
 @client.command()
 async def test(ctx, amount = 1):
@@ -163,17 +129,6 @@ async def pm(ctx, member: discord.Member, amount = 1):
 #member joined the server
 @client.event
 async def on_member_join(member):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
- 
- 
-    await update_data(users, member)
- 
-   
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-    
-    
     channel = client.get_channel(693929823030214658)
 
     role = discord.utils.get(member.guild.roles, id = 693933516294979704)
