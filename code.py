b3bb7cc769f43566dd8c6dd6d523a3c5dbd1b3bc
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-04-28 21:48:16 +0300

    Update code.py

--- code.py
+++ code.py
@@ -14,7 +14,7 @@ client = commands.Bot(command_prefix = "cephalon/")
 async def on_message(message):
     if 'discord.gg' in message.content.lower():
         await message.delete()
-        await message.channel.send('пашол нахуй со своей рекламой, {0}'.format(ctx.author.name))
+        await message.channel.send('пашол нахуй со своей рекламой, {0}'.format(member.mention))
     await client.process_commands(message)
 
 @client.command()
