62ee657247e14ca5f48d97244f30bca1360eb50a
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-05-02 23:56:04 +0300

    Update code.py

--- code.py
+++ code.py
@@ -92,7 +92,7 @@ async def on_raw_reaction_add(payload):
 
 @client.event
 async def on_raw_reaction_add(payload):
-    message_if = payload.message_id
+    message_id = payload.message_id
     if message_id == 695687657275260949:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guild)
