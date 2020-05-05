10cf91f420fc597a5484c931b674da98628cdad8
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-05-05 21:32:26 +0300

    Update code.py

--- code.py
+++ code.py
@@ -264,6 +264,7 @@ async def support(ctx, amount = 1):
     emb.add_field(name = '{}say'.format('cephalon/'), value = 'пишет сообщение от лица бота')
     emb.add_field(name = '{}coinflip'.format('cephalon/'), value = 'подкидывает монетку')
     emb.add_field(name = '{}gaystvo'.format('cephalon/'), value = 'как cephalon/say, но пингует @everyone')
+    emb.add_field(name = '{}embed'.format('cephalon/'), value = 'как cephalon/say, но пишет через эмбед')
     emb.add_field(name = 'жыж', value = 'также, для написания команд необязательно писать префикс, можно пингануть бота')
     emb.set_footer(text = 'Cephalon Cy by сасиска#2472')
     await ctx.send(embed = emb)
