d4ad87ae71b6121b3400a8a9a462809ebabcc981
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-05-06 19:45:03 +0300

    Update code.py

--- code.py
+++ code.py
@@ -132,11 +132,6 @@ async def on_reaction_add(reaction, user):
     channel = 707492082209652827
     await ctx.send(channel, '**{}** Добавил {} к сообщению ```{}```'.format(user.name, reaction.emoji, reaction.message.content)
 
-@client.event
-async def on_reaction_add(reaction, user):
-    channel = 707492082209652827
-    await ctx.send(channel, '**{}** Добавил {} к сообщению ```{}```'.format(user.name, reaction.emoji, reaction.message.content)
-
 #альтернатива Groovy(которая, сука не работает)
 @client.command()
 async def join(ctx):
