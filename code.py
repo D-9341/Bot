608c8c6da7a507e0fc70343cef752f444dc0e3c1
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-05-07 19:07:47 +0300

    Update code.py

--- code.py
+++ code.py
@@ -62,6 +62,39 @@ async def coinflip(ctx, amount = 1):
 @client.event
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
+    if message_id == 707985429944860773:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+
+        if payload.emoji.name == 'Discord':
+            role = discord.utils.get(guild.roles, name = 'Discord')
+        elif payload.emoji.name == 'YouTube':
+            role = discord.utils.get(guild.roles, name = 'YouTube')
+        if role is not None:
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            if member is not None:
+                await member.add_roles(role)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    message_id = payload.message_id
+    if message_id == 707985429944860773:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+
+        if payload.emoji.name == 'Discord':
+            role = discord.utils.get(guild.roles, name = 'Discord')
+        elif payload.emoji.name == 'YouTube':
+            role = discord.utils.get(guild.roles, name = 'YouTube')
+
+        if role is not None:
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            if member is not None:
+                await member.remove_roles(role)
+
+@client.event
+async def on_raw_reaction_add(payload):
+    message_id = payload.message_id
     if message_id == 707496056505761802:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
