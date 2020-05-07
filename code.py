30624f09822924b21751b9b1a174046c7ebdb920
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-05-07 15:13:38 +0300

    Update code.py

--- code.py
+++ code.py
@@ -62,7 +62,7 @@ async def coinflip(ctx, amount = 1):
 @client.event
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
-    if message_id == 707919456948715541:
+    if message_id == 707928017627906069:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
         
@@ -79,7 +79,7 @@ async def on_raw_reaction_add(payload):
 @client.event
 async def on_raw_reaction_remove(payload):
     message_id = payload.message_id
-    if message_id == 707919456948715541:
+    if message_id == 707928017627906069:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
         
