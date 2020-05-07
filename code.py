97d9e87593ab27aa4d71f83f27612b405b68bf0f
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-05-07 11:35:29 +0300

    Update code.py

--- code.py
+++ code.py
@@ -30,7 +30,7 @@ async def mute(ctx, member: discord.Member, amount = 1):
 @commands.has_permissions(administrator = True)
 async def embed(ctx, *, arg, amount = 1):
     await ctx.channel.purge(limit = amount)
-    emb = discord.Embed(title = ctx.author.name, colour = discord.Color.orange())
+    emb = discord.Embed(title = f'{} {}'.format(client.user.avatar_url, ctx.author.name), colour = discord.Color.orange())
     emb.add_field(name = 'SPELL', value = arg)
     emb.set_footer(text = 'Cephalon Cy by сасиска#2472')
     await ctx.send(embed = emb)
