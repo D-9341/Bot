61e5d7677ba2ad5ace937199efdc73eab4f42372
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-05-11 09:08:37 +0300

    Update code.py

--- code.py
+++ code.py
@@ -164,7 +164,7 @@ async def on_command_error(ctx, error):
 @commands.has_permissions(administrator = True)
 async def pm(ctx, arg, *, member: discord.Member, amount = 1):
     await ctx.channel.purge(limit = amount)
-    await member.send(arg)
+    await member.send('Адамант сука')
 
 #member joined the server
 @client.event
@@ -213,7 +213,7 @@ async def time(ctx, amount = 1):
 #проверка на подключение
 @client.event
 async def on_ready():
-    await client.change_presence(status = discord.Status.online, activity = discord.Game('ни во что'))
+    await client.change_presence(status = discord.Status.idle, activity = discord.ActivityType.watching('дебилами'))
 
 #kick
 @client.command()
@@ -259,6 +259,13 @@ async def gaystvo_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send(f'{ctx.author.name}, u')
 
+@say.error
+async def say_error(ctx, error):
+    if isinstance(errorm commands.MissingRequiredArgument):
+        await ctx.send(f'{ctx.author.name}, чё сказать то?')
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f'{ctx.author.name}, u')
+        
 @mute.error
 async def mute_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
