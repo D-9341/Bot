b91421acda9ae7f0ad6263e8294e85349f650d9f
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-05-24 21:31:50 +0300

    Update code.py

--- code.py
+++ code.py
@@ -19,7 +19,7 @@ async def info(ctx, amount = 1):
 @client.event
 async def on_raw_message_delete(payload, message):
     channel = client.get_channel(714175791033876490)
-    await channel.send(message.content + f' was deleted, author - {ctx.author.name}')
+    await channel.send(f'{message.content} was deleted, author - {ctx.author.name}')
     
 @client.command()
 @commands.has_permissions(administrator = True)
