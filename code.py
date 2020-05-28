c39b5bdc3af688b35b9230b8a519c37178d75c92
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-05-28 20:35:57 +0300

    Update code.py

--- code.py
+++ code.py
@@ -17,7 +17,7 @@ async def info(ctx, amount = 1):
     await ctx.send(f'Cephalon online, Ping equals `{round(client.latency * 1000)} ms`')
   
 @client.event
-async def on_raw_message_delete(payload):
+async def on_message_delete(ctx):
     channel = client.get_channel(714175791033876490)
     if ctx.author.name != 'Groovy':
         await channel.send(f'```{ctx.content}``` was deleted, author - {ctx.author.name}')
