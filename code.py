eae17db4c423d1225b0326c1646e0f6c75fa847a
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-06-29 09:51:42 +0300

    Update code.py

--- code.py
+++ code.py
@@ -17,10 +17,9 @@ async def info(ctx, amount = 1):
     await ctx.send(f'Cy в сети, пинг равен `{round(client.latency * 1000)} ms`')
 
 @client.event
-async def on_raw_message_delete(ctx):
+async def on_raw_message_delete(payload):
     channel = client.get_channel(714175791033876490)
-    if ctx.author.name != 'Groovy':
-        await channel.send(f'```{ctx.content}``` было удалено, автор - {ctx.author.mention}')
+    await channel.send('что-то было удалено. что, я не знаю')
 
 @client.command()
 @commands.has_permissions(administrator = True)
