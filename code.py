00aee616b700bc4c62a70d8945fe3cf57ec2eba2
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-07-03 17:13:53 +0300

    Update code.py

--- code.py
+++ code.py
@@ -12,7 +12,7 @@ client.remove_command('help')
 
 #test commands space
 @client.event
-async def on_raw_message_delete(payload):
+async def on_raw_message_delete(payload, message):
     channel = client.get_channel(id = 714175791033876490)
     if message.author != 'Groovy':
         await channel.send(f'{message.content} было удалено, автор - {message.author.mention}')
