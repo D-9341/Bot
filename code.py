c286133131d70b8d22833becb4a7689c9218f249
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-07-03 09:58:11 +0300

    Update code.py

--- code.py
+++ code.py
@@ -40,7 +40,7 @@ async def about(ctx, member:discord.Member, amount = 1):
 @client.event
 async def on_voice_state_update(member,before,after):
     if after.channel.id == 694212304165929101:
-        channel = await create_voice_channel(name = f'{member.name}', category = None)
+        channel = await after.create_voice_channel(name = f'{member.name}', category = None)
         await member.move_to(channel)
         await channel.set_permissions(member,mute_members=True,move_members=True,manage_channels=True)
         def check(a,b,c):
