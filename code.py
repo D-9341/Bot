72b1d03559d39c5d06f74728c3dd634eae13ce4a
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-07-12 19:15:43 +0300

    Update code.py

--- code.py
+++ code.py
@@ -11,14 +11,18 @@ client.remove_command('help')
 #like cephalon/help
 
 #test commands space
-
+@client.command()
+async def give(ctx, member : dicord.Member, amount = 1):
+    await ctx.channel.purge(limit = amount)
+    role = discord.utils.get(ctx.message.guild.roles, name = 'Командир ВВС-"Израиль"')
+    await member.add_roles(role)
 #test commands space
 
 @client.command()
 @commands.has_permissions(administrator = True)
 async def info(ctx, amount = 1):
     await ctx.channel.purge(limit = amount)
-    await ctx.send(f'Cy в сети, пинг равен `{round(client.latency * 1000)} ms`, это моя 427 версия')
+    await ctx.send(f'Cy в сети, пинг равен `{round(client.latency * 1000)} ms`, это моя 428 версия')
 
 @client.command()
 @commands.has_permissions(administrator = True)
