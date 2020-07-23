52295bbb26ee49b2d13c9c715594394c97b61184
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-07-23 18:09:51 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ import os
 import discord
 from discord.ext import commands
 from discord.utils import get
+
 client = commands.Bot(command_prefix = commands.when_mentioned_or('cephalon/'))
 client.remove_command('help')
 
