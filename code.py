ce80c3bbaf86bd5683917d18fbb33b5d6cce0ee8
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-07-30 22:42:00 +0300

    Update code.py

--- code.py
+++ code.py
@@ -192,13 +192,14 @@ async def pm(ctx, member: discord.Member, amount = 1):
 
 @client.event
 async def on_member_join(member):
-    channel = client.get_channel(693929823030214658)
-
     role = discord.utils.get(member.guild.roles, id = 693933516294979704)
     role1 = discord.utils.get(member.guild.roles, id = 693933510523879454)
     role2 = discord.utils.get(member.guild.roles, id = 693933514198089838)
-
     await member.add_roles(role, role1, role2)
+    
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(693929823030214658)
     await channel.send(embed = discord.Embed(description = f'{member.name} Зашёл в комплекс, 👋', colour = discord.Color.orange()))
 
 @client.event
