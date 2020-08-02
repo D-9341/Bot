90955f69b9feb2d3208dc485b07b745ec23d59d2
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-02 19:26:17 +0300

    Update code.py

--- code.py
+++ code.py
@@ -11,15 +11,15 @@ client.remove_command('help')
 
 #test commands space
 @client.event
-async def on_voice_state_update(member,before,after):
+async def on_voice_state_update(member,before,after, guild : discord.Guild):
     if after.channel.id == 739518627115565159:
         mainCategory = discord.utils.get(after.channel.guild.categories, id=693937532550774824)
-        channel2 = await after.guild.create_voice_channel(name=f"Комната {member.display_name}",category=mainCategory)
+        channel2 = await guild.create_voice_channel(name=f"Комната {member.display_name}",category=mainCategory)
         await member.move_to(channel2)
         await channel2.set_permissions(member,mute_members=True,move_members=True,manage_channels=True)
         def check(a,b,c):
             return len(channel2.members) == 0
-        await Bot.wait_for('voice_state_update', check=check)
+        await client.wait_for('voice_state_update', check=check)
         await channel2.delete()
 #test commands space
 
