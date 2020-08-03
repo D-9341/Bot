f359e90c2c39ae9a1240d89d862ca35ebaa3dba1
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-03 17:29:26 +0300

    Update code.py

--- code.py
+++ code.py
@@ -13,8 +13,8 @@ client.remove_command('help')
 @client.command
 async def edit(ctx, arg, arg1, *, amount = 1):
     await ctx.channel.purge(limit = amount)
-    await fetch_message(arg)
-    await message.edit(arg1)
+    m = client.get_message(arg)
+    await m.edit(arg1)
 #test commands space
 
 @client.command(aliases = ['Info', 'INFO'])
