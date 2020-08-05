c32579c1c213915c4af05c257223e097c2a75abf
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-05 20:38:24 +0300

    Update code.py

--- code.py
+++ code.py
@@ -81,7 +81,7 @@ async def mute(ctx, member: discord.Member, time : int, amount = 1):
                 await ctx.send(embed = emb)
                 await member.remove_roles(mrole)
             else:
-                await ctx.send(f'{ctx.author.mention}, Я не могу снять мут у {member.name} из-за того, что роль Muted была удалена/отредактирована!')
+                await ctx.send(f'{ctx.author.mention}, Я не могу снять мут у {member.mention} из-за того, что роль Muted была удалена/отредактирована!')
         else:
             await ctx.send(f'{ctx.author.mention}, Я не смог найти подходящую для этой команды роль. Роль должна называться Muted')
     else:
