0497a95ddff62982dec7829087fd1712d45064b4
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-09 21:44:36 +0300

    Update code.py

--- code.py
+++ code.py
@@ -17,7 +17,7 @@ async def on_message_edit(before, after):
     channel = client.get_channel(714175791033876490)
     if channel is None:
         return
-    if not ctx.author.bot:
+    if not before.message.author.bot:
         emb = discord.Embed(title = 'Сообщение было изменено', colour = discord.Color.orange(), timestamp = datetime.datetime.utcnow())
         emb.set_author(name = before.message.author.name, icon_url = before.message.author.name)
         emb.add_field(name = 'Было', value = before.message.content)
