b1aec5d45f5330ea8a724453323f77f3581527b0
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-10 22:23:17 +0300

    Update code.py

--- code.py
+++ code.py
@@ -16,7 +16,7 @@ client.owner_id = 338714886001524737
 async def on_voice_state_update(member, after, before):
     if before.channel.id == 742457421456343272:
         category = discord.utils.get(before.channel.guild.categories, id = 693937532550774824)
-        channel2 = await guild.create_voice_channel(name = f'Комната {member.name}', category = category)
+        channel2 = await after.guild.create_voice_channel(name = f'Комната {member.name}', category = category)
         await channel2.set_permissions(member, mute_members = True, move_members = True, manage_channels = True)
         await member.move_to(channel2)
         def check(a, b, c):
