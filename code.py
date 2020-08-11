80bbd641a1d89357c40c607031b14d2f7d85ae80
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-11 22:23:42 +0300

    Update code.py

--- code.py
+++ code.py
@@ -143,7 +143,7 @@ async def mute(ctx, member: discord.Member, time : int, arg, *, amount = 1):
                         emb = discord.Embed(description = f'{member.mention} уже размучен, мут не был автоматически снят', colour = discord.Color.orange())
                         emb.set_footer(text = 'Cephalon Cy от сасиска#2472. Secured by Knox')
                         await asyncio.sleep(1)
-                        await ctx.send(embed = emb, delete_after = 10)
+                        await ctx.send(embed = emb, delete_after = 4)
             else:
                 emb = discord.Embed(description = f'{ctx.author.mention}, Я не могу снять мут у {member.mention} из-за того, что роль Muted была удалена/отредактирована!', colour = discord.Color.orange())
                 emb.set_footer(text = 'Cephalon Cy от сасиска#2472. Secured by Knox')
