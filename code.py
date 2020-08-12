54654c570e79c0b1a189b08527c7bedb7f56d4c7
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-12 20:19:22 +0300

    Update code.py

--- code.py
+++ code.py
@@ -12,7 +12,7 @@ client.remove_command('help')
 client.owner_id = 338714886001524737
 
 #test commands space
-@client.command
+@client.command()
 async def purge(ctx, amount : int):
     await ctx.channel.purge(limit = amount + 1)
     if amount.endswith('1') and amount != 11:
