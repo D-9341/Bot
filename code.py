a66600c8f34b752f122bbcd17f26318ed3fb466f
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-16 19:14:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -23,7 +23,8 @@ async def info(ctx, amount = 1):
     emb.add_field(name = 'Написан на', value = 'discord.py')
     emb.add_field(name = 'Разработчик', value = 'Написано в футере, ха!')
     emb.add_field(name = 'Веб-сайт', value = 'http://ru-unioncraft.ru/')
-    emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
+    emb.add_field(name = 'Шард', value = client.shard_id)
+    emb.set_author(name = client.user.name, icon_url = client.user.avatar_url)
     emb.set_footer(text = 'Cephalon Cy от сасиска#2472. Secured by Knox')
     await ctx.send(embed = emb)
     
