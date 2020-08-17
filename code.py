2f90d45e98ead934ac28193c74d361d4cde5a530
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-17 23:23:28 +0300

    Update code.py

--- code.py
+++ code.py
@@ -20,12 +20,12 @@ client.owner_id = 338714886001524737
 async def info(ctx, amount = 1):
     await ctx.channel.purge(limit = amount)
     emb = discord.Embed(title = 'Привет! Я Cephalon Cy!', colour = discord.Color.orange())
+    emb.set_author(name = '[Cy](https://warframe.fandom.com/ru/wiki/Цефалон_Сай)', icon_url = client.user.avatar_url)
     emb.add_field(name = 'Версия', value = '0.12.7.8631')
     emb.add_field(name = 'Написан на', value = 'discord.py')
     emb.add_field(name = 'Разработчик', value = 'Написано в футере, ха!')
     emb.add_field(name = 'Веб-сайт', value = '```http://ru-unioncraft.ru/```')
     emb.add_field(name = 'Шард', value = client.shard_count)
-    emb.set_author(name = client.user.name, icon_url = client.user.avatar_url)
     emb.set_footer(text = 'Cephalon Cy от сасиска#2472. Secured by Knox')
     await ctx.send(embed = emb)
     
