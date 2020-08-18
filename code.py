8e57ee3926a28e9e5017ae6aa4789181d1ab2358
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-18 19:45:38 +0300

    Update code.py

--- code.py
+++ code.py
@@ -22,10 +22,10 @@ async def embed(ctx, d, t, img = None, f = None, a = None, *, amount = 1):
         a = int('0x' + a, 16)
         
     if img == None:
-        img = 'https://steamcommunity.com/profiles/ЦИФРЫ/'
+        img = ('https://steamcommunity.com/profiles/ЦИФРЫ/')
         
     if f == None:
-        img = 'https://steamcommunity.com/profiles/ЦИФРЫ/'
+        img = ('https://steamcommunity.com/profiles/ЦИФРЫ/')
         
     emb = discord.Embed(colour = a)
     emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
