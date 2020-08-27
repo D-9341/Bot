f9ea38f241385599ee738dbd1c283fef65fdab2f
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-08-27 21:01:46 +0300

    Update code.py

--- code.py
+++ code.py
@@ -238,16 +238,13 @@ async def take(ctx, member: discord.Member, *, arg, amount = 1):
 @client.command(aliases = ['emb_e'])
 @commands.has_permissions(mention_everyone = True)
 @commands.cooldown(1, 20, commands.BucketType.default)
-async def everyone_embed(ctx, t, d, img = None, f = None, au:discord.Member = None, a = None, fu = None, *, amount = 1):
+async def everyone_embed(ctx, t, d, img = None, f = None, a = None, fu = None, *, amount = 1):
     await ctx.channel.purge(limit = amount)
     await ctx.send('@everyone')
     if a == None:
         a = ctx.author.color
     else:
         a = int('0x' + a, 16)
-
-    if au == None:
-        au = ctx.author
         
     if fu == None:
         fu = ('Cephalon Cy от сасиска#2472. Secured by Knox')
@@ -259,7 +256,7 @@ async def everyone_embed(ctx, t, d, img = None, f = None, au:discord.Member = No
         f = ('https://steamcommunity.com/profiles/ЦИФРЫ/')
         
     emb = discord.Embed(title = t, description = d, colour = a)
-    emb.set_author(name = au, icon_url = au.avatar_url)
+    emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
     emb.set_image(url = img)
     emb.set_thumbnail(url = f)
     emb.set_footer(text = fu)
@@ -267,15 +264,12 @@ async def everyone_embed(ctx, t, d, img = None, f = None, au:discord.Member = No
     
 @client.command(aliases = ['Embed', 'EMBED', 'emb' , 'Emb', 'EMB'])
 @commands.has_permissions(manage_channels = True)
-async def embed(ctx, t, d, img = None, f = None, au:discord.Member = None, a = None, fu = None, *, amount = 1):
+async def embed(ctx, t, d, img = None, f = None, a = None, fu = None, *, amount = 1):
     await ctx.channel.purge(limit = amount)
     if a == None:
         a = au.color
     else:
         a = int('0x' + a, 16)
-
-    if au == None:
-        au = ctx.author
 
     if fu == None:
         fu = ('Cephalon Cy от сасиска#2472. Secured by Knox')
@@ -287,7 +281,7 @@ async def embed(ctx, t, d, img = None, f = None, au:discord.Member = None, a = N
         f = ('https://steamcommunity.com/profiles/ЦИФРЫ/')
         
     emb = discord.Embed(title = t, description = d, colour = a)
-    emb.set_author(name = au, icon_url = au.avatar_url)
+    emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
     emb.set_image(url = img)
     emb.set_thumbnail(url = f)
     emb.set_footer(text = fu)
@@ -295,16 +289,13 @@ async def embed(ctx, t, d, img = None, f = None, au:discord.Member = None, a = N
 
 @client.command(aliases = ['emb_ed'])
 @commands.has_permissions(manage_channels = True)
-async def emb_edit(ctx, arg, t, d, img = None, f = None, au:discord.Member = None, a = None, fu = None, *, amount = 1):
+async def emb_edit(ctx, arg, t, d, img = None, f = None, a = None, fu = None, *, amount = 1):
     await ctx.channel.purge(limit = amount)
     m = await ctx.fetch_message(id = arg)
     if a == None:
         a = ctx.author.color
     else:
         a = int('0x' + a, 16)
-
-    if au == None:
-        au = ctx.author
 
     if fu == None:
         fu = ('Cephalon Cy от сасиска#2472. Secured by Knox')
@@ -316,7 +307,7 @@ async def emb_edit(ctx, arg, t, d, img = None, f = None, au:discord.Member = Non
         f = ('https://steamcommunity.com/profiles/ЦИФРЫ/')
         
     emb = discord.Embed(title = t, description = d, colour = a)
-    emb.set_author(name = au, icon_url = au.avatar_url)
+    emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
     emb.set_image(url = img)
     emb.set_thumbnail(url = f)
     emb.set_footer(text = fu)
