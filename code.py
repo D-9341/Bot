da9bf4e85e041d60ced2e27867703bac5c5f79f2
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-09-04 16:37:29 +0300

    Update code.py

--- code.py
+++ code.py
@@ -12,7 +12,10 @@ client.remove_command('help')
 client.owner_id = 338714886001524737
 
 #test commands space
-
+@client.command
+async def report(ctx, arg, member : discord.Member = None):
+    member.id = client.owner_id
+    await member.send(f'{ctx.author.mention} отправил вам репорт с текстом: {arg}')
 #test commands space
 
 @client.command(aliases = ['Info', 'INFO'])
