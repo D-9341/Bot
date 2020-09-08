65563f46502b85700f73539788ae2a5b62fb9fee
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-09-08 19:39:47 +0300

    Update code.py

--- code.py
+++ code.py
@@ -16,7 +16,7 @@ client.owner_id = 338714886001524737
 async def emb_content(ctx, arg):
     await ctx.message.delete()
     message = await ctx.fetch_message(id = arg)
-    await ctx.send(f'{message.title}')
+    await ctx.send(f'{message.embeds}')
 #test commands space
 
 @client.command(aliases = ['.пуленепробиваемое-стекло'])
