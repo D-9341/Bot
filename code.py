c221cb44d9aed3c380651bb2eacd27ddb8c2226d
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-09-09 22:30:09 +0300

    Update code.py

--- code.py
+++ code.py
@@ -125,7 +125,7 @@ async def guild(ctx, guild : discord.Guild = None):
 async def role(ctx, *, arg):
     await ctx.message.delete()
     guild = ctx.guild
-    role = discord.utils.get(guild.roles, name = arg)
+    role = discord.utils.get(guild.roles, name = arg or mention = arg)
     emb = discord.Embed(title = role.name, colour = ctx.author.color)
     emb.add_field(name = 'ID', value = role.id)
     emb.add_field(name = 'Цвет', value = role.color)
