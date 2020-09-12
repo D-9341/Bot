2718d04efa4ff602634573a61e41274940f98da7
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-09-12 10:38:51 +0300

    Update code.py

--- code.py
+++ code.py
@@ -11,7 +11,20 @@ client.remove_command('help')
 client.owner_id = 338714886001524737
 
 #test commands space
-
+@client.command()
+async def zatka(ctx):
+    await ctx.message.delete()
+    emb = discord.Embed(title = 'Форма заявки для Набор кадров', colour = ctx.author.color)
+    emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
+    emb.add_field(name = '(1). ZATKA в STEAM.  ZATKA_KING#8406 в Discord.', value = 'возраст 14+  часовой пояс IL +0.', inline = False)
+    emb.add_field(name = '(2). Интересующая управление:', value = 'Discord', inline = False)
+    emb.add_field(name = '(3). Опыт администрирования:', value = 'Есть.', inline = False)
+    emb.add_field(name = 'творческие:', value = 'Есть.', inline = False)
+    emb.add_field(name = 'технические навыки:', value = 'Нет.', inline = False)
+    emb.add_field(name = '(4). Сколько часов готовы уделять работе', value = '[ 15+ в неделю ]', inline = False)
+    emb.add_field(name = 'в какое время дня свободны', value = '16:00 до 22:00+', inline = False)
+    emd.set_footer(text = 'Cephalon Cy by сасиска#2472')
+    await ctx.send(embed = emb)
 #test commands space
 
 @client.command(aliases = ['.пуленепробиваемое-стекло'])
