a9c32b52c304d1307213c783f8499b784432245c
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-09-17 22:34:54 +0300

    Update code.py

--- code.py
+++ code.py
@@ -527,7 +527,7 @@ async def on_member_join(member):
         role1 = discord.utils.get(member.guild.roles, id = 693933511412940800)
         if role is not None:
             await member.add_roles(role, role1)
-        emb = discord.Embed(description = f'А, {member.mention} ({member.name}) - очередной ботяра? ок', colour = discord.Color.orange())
+        emb = discord.Embed(description = f'А, {member.mention} ({member.name}) - очередной ботяра? `{member.guild.name}`, зачем?', colour = discord.Color.orange())
         await channel.send(embed = emb)
 
 @client.event
@@ -538,7 +538,7 @@ async def on_member_remove(member):
         emb.set_footer(text = 'Cephalon Cy by сасиска#2472')
         await channel.send(embed = emb)
     elif member.bot != False and member.id != 694170281270312991:
-        emb = discord.Embed(description = f'{member.mention} ({member.name}), ну и вали, ботаря, хаха!', colour = discord.Color.orange())
+        emb = discord.Embed(description = f'{member.mention} ({member.name}), ну и с `{member.guild.name}` вали, ботаря, хаха!', colour = discord.Color.orange())
         emb.set_footer(text = 'Cephalon Cy by сасиска#2472')
         await channel.send(embed = emb)
     elif member.bot != False and member.id == 694170281270312991:
