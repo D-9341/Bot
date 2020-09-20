550dd7ad56f9066ee174eb344efb64117974e13b
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-09-20 21:41:33 +0300

    Add files via upload

---
+++ sometest.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class test(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def pong(self, ctx):
+        await ctx.send('Ping!')
+
+
+def setup(client):
+    client.add_cog(test(client))
