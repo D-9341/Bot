d7b6fa9a724734c99f074c839e199afe1ec1ef01
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-09-23 12:47:54 +0300

    Update Misc.py

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 class Misc(commands.Cog):
-    def __init__(client):
+    def __init__(self, client):
         self.client = client
         
     @commands.Cog.listener()
