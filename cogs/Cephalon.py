9bc01cbcae305c9f616fea7c5011aee8f4c2e2c1
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-09-24 20:55:43 +0300

    Update Cephalon.py

--- cogs/Cephalon.py
+++ cogs/Cephalon.py
@@ -15,7 +15,7 @@ class Cephalon(commands.Cog):
         await ctx.message.delete()
         emb = discord.Embed(title = 'Welcome to the cum zone', colour = discord.Color.orange())
         emb.set_author(name = self.client.user.name, url = 'https://warframe.fandom.com/wiki/Cephalon_Cy', icon_url = self.client.user.avatar_url)
-        emb.add_field(name = 'Версия', value = '0.12.7.8824')
+        emb.add_field(name = 'Версия', value = '0.12.7.9018')
         emb.add_field(name = 'Написан на', value = 'discord.py')
         emb.add_field(name = 'Разработчик', value = 'Написано в футере, ха!')
         emb.add_field(name = 'Веб-сайт', value = '```http://ru-unioncraft.ru/```')
