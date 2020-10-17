fb4e22aeab1ddbbb1105cee2a1e243f310b66b7a
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-10-17 23:43:44 +0300

    Update Misc.py

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -31,21 +31,34 @@ class Misc(commands.Cog):
     @commands.command(aliases = ['Guild', 'GUILD'])
     @commands.cooldown(1, 5, commands.BucketType.default)
     async def guild(self, ctx, guild: discord.Guild = None):
-        if guild == None:
-            guild = ctx.guild
         await ctx.message.delete()
-        emb = discord.Embed(title = f'Информация о {guild}', colour = discord.Color.orange(), timestamp = ctx.message.created_at)
+        guild = ctx.guild
+        emb = discord.Embed(colour = discord.Color.orange(), timestamp = ctx.message.created_at)
+        emb.set_author(name = guild, icon_url = guild.icon_url)
         emb.add_field(name = 'ID сервера', value = guild.id)
-        emb.add_field(name = 'Уровень сервера', value = guild.premium_tier)
-        emb.add_field(name = 'Люди, бустящие сервер', value = guild.premium_subscribers)
-        emb.add_field(name = 'Человек', value = guild.member_count)
-        if len(guild.roles) >= 15:
-            emb.add_field(name = 'Роли', value = f'Слишком много ({len(guild.roles)-1})', inline = False)
+        emb.add_field(name = 'Голосовой регион', value = guild.region)
+        emb.add_field(name = 'Участников', value = guild.member_count)
+        emb.add_field(name = 'Каналов', value = f'Текстовых {len(guild.text_channels)} | Голосовых {len(guild.voice_channels)}')
+        limit = len(guild.roles)
+        if limit > 21:
+            emb.add_field(name = 'Роли', value = f'Слишком много для отрисовки ({len(guild.roles)-1}) [лимит 20]', inline = False)
+        elif limit == 21:
+            emb.add_field(name = f'Роли ({len(guild.roles)-1}) [лимит достигнут]', value = ', '.join([role.mention for role in guild.roles[1:]]), inline = False)
+        elif limit == 20:
+            emb.add_field(name = f'Роли ({len(guild.roles)-1}) [1 до лимита]', value = ', '.join([role.mention for role in guild.roles[1:]]), inline = False)
+        elif limit == 19:
+            emb.add_field(name = f'Роли ({len(guild.roles)-1}) [2 до лимита]', value = ', '.join([role.mention for role in guild.roles[1:]]), inline = False)
+        elif limit == 18:
+            emb.add_field(name = f'Роли ({len(guild.roles)-1}) [3 до лимита]', value = ', '.join([role.mention for role in guild.roles[1:]]), inline = False)
         else:
-            emb.add_field(name = f'Роли [{len(guild.roles)-1}]', value = ' '.join([role.mention for role in guild.roles[1:]]), inline = False)
-        emb.add_field(name = 'Дата создания сервера', value = guild.created_at.strftime('%d/%m/%Y %H:%M:%S UTC'), inline = False)
-        emb.set_thumbnail(url = guild.icon_url)
+            emb.add_field(name = f'Роли ({len(guild.roles)-1})', value = ', '.join([role.mention for role in guild.roles[1:]]), inline = False)
+        now = datetime.datetime.today()
+        then = guild.created_at
+        delta = now - then
+        d = guild.created_at.strftime('%d/%m/%Y %H:%M:%S UTC')
+        emb.add_field(name = 'Дата создания сервера', value = f'{delta.days} дней назад. ({d})', inline = False)
         emb.set_footer(text = 'Cephalon Cy by сасиска#2472')
+        emb.set_thumbnail(url = guild.icon_url)
         await ctx.send(embed = emb)
     
     @commands.command()
@@ -70,7 +83,11 @@ class Misc(commands.Cog):
         emb.add_field(name = 'Упоминается?', value = role.mentionable)
         emb.add_field(name = 'Управляется интеграцией?', value = role.managed)
         emb.add_field(name = 'Позиция в списке', value = role.position)
-        emb.add_field(name = 'Создана', value = role.created_at.strftime('%d/%m/%Y %H:%M:%S UTC'), inline = False)
+        now = datetime.datetime.today()
+        then = role.created_at
+        delta = now - then
+        d = role.created_at.strftime('%d/%m/%Y %H:%M:%S UTC')
+        emb.add_field(name = 'Создана', value = f'{delta.days} дня(ей) назад. ({d})', inline = False)
         emb.add_field(name = 'Показывает участников отдельно?', value = role.hoist)
         emb.set_footer(text = 'Cephalon Cy by сасиска#2472')
         await ctx.send(embed = emb)
@@ -81,9 +98,15 @@ class Misc(commands.Cog):
         await ctx.message.delete()
         if member == None:
             member = ctx.author
-        emb = discord.Embed(description = f'[Прямая ссылка]({member.avatar_url})', colour = member.color)
-        emb.set_author(name = member)
+        av = 'png'
+        av1 = 'webp'
+        av2 = 'jpg'
+        emb = discord.Embed(colour = member.color)
+        emb.add_field(name = '.png', value = f'[Ссылка]({member.avatar_url_as(format = av)})')
+        emb.add_field(name = '.webp', value = f'[Ссылка]({member.avatar_url_as(format = av1)})')
+        emb.add_field(name = '.jpg', value = f'[Ссылка]({member.avatar_url_as(format = av2)})')
         emb.set_image(url = member.avatar_url)
+        emb.set_author(name = member)
         emb.set_footer(text = 'Cephalon Cy by сасиска#2472')
         await ctx.send(embed = emb)
     
@@ -99,15 +122,34 @@ class Misc(commands.Cog):
             bot = 'Неа'
         elif member.bot == True:
             bot = 'Ага'
-        emb = discord.Embed(title = f'Информация о {member}', colour = member.color, timestamp = ctx.message.created_at)
+        emb = discord.Embed(colour = member.color, timestamp = ctx.message.created_at)
+        emb.set_author(name = member)
         emb.add_field(name = 'ID', value = member.id)
-        emb.add_field(name = 'Создан', value = member.created_at.strftime('%d/%m/%Y %H:%M:%S UTC'), inline = False)
-        emb.add_field(name = 'Вошёл', value = member.joined_at.strftime('%d/%m/%Y %H:%M:%S UTC'), inline = False)
+        now = datetime.datetime.today()
+        then = member.created_at
+        delta = now - then
+        d = member.created_at.strftime('%d/%m/%Y %H:%M:%S UTC')
+        then1 = member.joined_at
+        delta1 = now - then1
+        d1 = member.joined_at.strftime('%d/%m/%Y %H:%M:%S UTC')
+        emb.add_field(name = 'Создан', value = f'{delta.days} дня(ей) назад. ({d})', inline = False)
+        emb.add_field(name = 'Вошёл', value = f'{delta1.days} дня(ей) назад. ({d1})', inline = False)
         emb.add_field(name = 'Упоминание', value = member.mention)
-        emb.add_field(name = 'Имя', value = member.name)
+        emb.add_field(name = 'Raw имя', value = member.name)
         emb.add_field(name = 'Никнейм', value = member.nick)
-        emb.add_field(name = 'Статус', value = member.status)
-        emb.add_field(name = f'Роли [{len(member.roles)-1}]', value=' '.join([role.mention for role in member.roles[1:]]), inline = False)
+        limit = len(member.roles)
+        if limit > 21:
+            emb.add_field(name = 'Роли', value = f'Слишком много для отрисовки ({len(member.roles)-1}) [лимит 20]', inline = False)
+        elif limit == 21:
+            emb.add_field(name = f'Роли ({len(member.roles)-1}) [лимит достигнут]', value = ', '.join([role.mention for role in member.roles[1:]]), inline = False)
+        elif limit == 20:
+            emb.add_field(name = f'Роли ({len(member.roles)-1}) [1 до лимита]', value = ', '.join([role.mention for role in member.roles[1:]]), inline = False)
+        elif limit == 19:
+            emb.add_field(name = f'Роли ({len(member.roles)-1}) [2 до лимита]', value = ', '.join([role.mention for role in member.roles[1:]]), inline = False)
+        elif limit == 18:
+            emb.add_field(name = f'Роли ({len(member.roles)-1}) [3 до лимита]', value = ', '.join([role.mention for role in member.roles[1:]]), inline = False)
+        else:
+            emb.add_field(name = f'Роли ({len(member.roles)-1})', value = ', '.join([role.mention for role in member.roles[1:]]), inline = False)
         emb.add_field(name = 'Высшая Роль', value = member.top_role.mention, inline = False)
         emb.add_field(name = 'Бот?', value = bot)
         emb.set_thumbnail(url = member.avatar_url)
