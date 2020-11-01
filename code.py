775216a77001088d34e8b7e67efa11ea398916bc
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-11-01 13:17:03 +0300

    Update code.py

--- code.py
+++ code.py
@@ -346,8 +346,6 @@ async def mute(ctx, member: discord.Member, time: TimeConverter, *, reason: str 
                 await ctx.send(embed = emb1, delete_after = 3)
                 await asyncio.sleep(3)
                 await member.add_roles(role)
-				if reason == None:
-                    reason = 'Не указана.'
                 emb = discord.Embed(colour = member.color, timestamp = ctx.message.created_at)
                 emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
                 emb.add_field(name = 'В муте', value = f'{member.mention}')
