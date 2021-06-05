de9ea996274e83ff8368ecab0d7a657af97af63d
D-9341 <58139230+D-9341@users.noreply.github.com>
2021-06-05 11:31:46 +0300

    Обновлено под нынешние реалии

--- cogs/Embeds.py
+++ cogs/Embeds.py
@@ -1,6 +1,11 @@
+import asyncio
+
 import discord
 from discord.ext import commands
-from discord.utils import get
+
+friends = [351071668241956865, 417362845303439360]
+
+botversions = [764882153812787250, 694170281270312991, 762015251264569352]
 
 class Embeds(commands.Cog):
     def __init__(self, client):
@@ -8,130 +13,176 @@ class Embeds(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Дополнение Embeds успешно загружено.')
-        
-    @commands.command(aliases = ['ctx'])
-    @commands.cooldown(1, 5, commands.BucketType.default)
-    async def content(self, ctx, arg):
-        await ctx.message.delete()
-        message = await ctx.fetch_message(id = arg)
-        if message.author == self.client.user:
-            await ctx.send(f'```cy/say noembed "{message.content}"```')
-        elif message.author.id == 762015251264569352:
-            await ctx.send(f'```cy\\say noembed "{message.content}"```')
-        else:
-            await ctx.send(f'```{message.content}```')
+        print('Модуль Embeds загружен')
 
-    @commands.command(aliases = ['emb_ctx'])
-    @commands.cooldown(1, 5, commands.BucketType.default)
-    async def emb_content(self, ctx, arg):
-        await ctx.message.delete()
-        message = await ctx.fetch_message(id = arg)
-        for emb in message.embeds:
-            if message.author == self.client.user:
-                await ctx.send(f'```cy/say "" "" t& {emb.title} d& {emb.description} f& {emb.footer.text} c& {emb.colour} a& {emb.author.name} img& {emb.image.url} fu& {emb.thumbnail.url}```')
-            elif message.author.id == 762015251264569352:
-                await ctx.send(f'```cy\\say "" "" t& {emb.title} d& {emb.description} f& {emb.footer.text} c& {emb.colour} a& {emb.author.name} img& {emb.image.url} fu& {emb.thumbnail.url}```')
+    @commands.command()
+    async def say(self, ctx, *, msg):
+        title = ''
+        description = ''
+        image = thumbnail = message = footer = None
+        embed_values = msg.split('|')
+        for i in embed_values:
+            if i.strip().lower().startswith('t&'):
+                title = i.strip()[2:].strip()
+            elif i.strip().lower().startswith('d&'):
+                description = i.strip()[2:].strip()
+            elif i.strip().lower().startswith('img&'):
+                image = i.strip()[4:].strip()
+            elif i.strip().lower().startswith('th&'):
+                thumbnail = i.strip()[3:].strip()
+            elif i.strip().lower().startswith('msg&'):
+                message = i.strip()[4:].strip()
+            elif i.strip().lower().startswith('f&'):
+                footer = i.strip()[2:].strip()
+        emb = discord.Embed(title = title, description = description, color = 0x2f3136)
+        for i in embed_values:
+            emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
+            if image:
+                emb.set_image(url = image)
+            if thumbnail:
+                emb.set_thumbnail(url = thumbnail)
+            if footer:
+                emb.set_footer(text = footer)
+            if ctx.guild.owner.id != self.client.owner_id and ctx.guild.owner.id not in friends:
+                emb.set_footer(text = 'Cephalon Cy by сасиска#2472')
+            if 't&' not in msg and 'd&' not in msg and 'img&' not in msg and 'th&' not in msg and 'msg&' not in msg and 'f&' not in msg:
+                return await ctx.send(msg)
             else:
-                await ctx.send(f'```title {emb.title} description {emb.description} footer {emb.footer.text} color {emb.colour} author {emb.author.name} image {emb.image.url} footer img {emb.thumbnail.url}```')
-            
-    @commands.command(aliases = ['emb_e'])
-    @commands.has_permissions(mention_everyone = True)
-    @commands.cooldown(1, 20, commands.BucketType.default)
-    async def say_everyone(self, ctx, arg = None, text = None, t = None, d = None, fu = None, img = None, f = None, c = None, a : discord.Member = None):
-        await ctx.message.delete()
-        if c == None:
-            c = ctx.author.color
-        else:
-            c = int('0x' + c, 16)
-        if a == None:
-            a = ctx.author
-        if img == None:
-            img = ('')
-        if f == None:
-            f = ('')
-        if fu == None:
-            fu = ('Cephalon Cy by сасиска#2472')
-        emb = discord.Embed(title = t, description = d, colour = c)
-        emb.set_author(name = a, icon_url = a.avatar_url)
-        emb.set_image(url = img)
-        emb.set_thumbnail(url = f)
-        emb.set_footer(text = fu)
-        if arg == 'noembed':
-            await ctx.send('@everyone ' + text)
-        elif arg != 'noembed':
-            await ctx.send('@everyone', embed = emb)
-    
-    @commands.command(aliases = ['Say', 'SAY'])
-    @commands.has_permissions(manage_channels = True)
-    async def say(self, ctx, arg = None, text = None, t = None, d = None, fu = None, img = None, f = None, c = None, a : discord.Member = None, *, role: discord.Role = None):
-        await ctx.message.delete()
-        if c == None:
-            c = ctx.author.color
-        else:
-            c = int('0x' + c, 16)
-        if a == None:
-            a = ctx.author
-        if img == None:
-            img = ('')
-        if f == None:
-            f = ('')
-        if role != None:
-            c = role.color
-        if fu == None:
-            fu = ('Cephalon Cy by сасиска#2472')
-        emb = discord.Embed(title = t, description = d, colour = c)
-        emb.set_author(name = a, icon_url = a.avatar_url)
-        emb.set_image(url = img)
-        emb.set_thumbnail(url = f)
-        emb.set_footer(text = fu)
-        if role is not None and arg != 'noembed':
-            await ctx.send(f'{role.mention}', embed = emb)
-        elif role is None and arg != 'noembed':
-            await ctx.send(embed = emb)
-        if arg == 'noembed':
-            await ctx.send(text)
+                if message:
+                    return await ctx.send(f'{message}', embed = emb)
+                else:
+                    return await ctx.send(embed = emb)
 
-    @commands.command(aliases = ['emb_ed'])
-    @commands.has_permissions(manage_channels = True)
-    async def emb_edit(self, ctx, arg, t = None, d = None, fu = None, img = None, f = None, c = None, a : discord.Member = None):
-        await ctx.message.delete()
+    @commands.command()
+    async def edit(self, ctx, arg, *, msg = None):
         message = await ctx.fetch_message(id = arg)
-        if c == None:
-            c = ctx.author.color
+        if message != None:
+            old_embed = message.embeds[0]
+            title = old_embed.title
+            description = old_embed.description
+            image = thumbnail = color = None
+            embed_values = msg.split('|')
+            for i in embed_values:
+                if i.strip().lower().startswith('t&'):
+                    title = i.strip()[2:].strip()
+                elif i.strip().lower().startswith('d&'):
+                    description = i.strip()[2:].strip()
+                elif i.strip().lower().startswith('img&'):
+                    image = i.strip()[4:].strip()
+                elif i.strip().lower().startswith('th&'):
+                    thumbnail = i.strip()[3:].strip()
+                elif i.strip().lower().startswith('c&'):
+                    color = i.strip()[2:].strip()
+            if color == None:
+                color = 0x2f3136
+            else:
+                color = int('0x' + color, 16)
+            emb = discord.Embed(title = title, description = description, color = color, timestamp = ctx.message.created_at)
+            for i in embed_values:
+                emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
+                if image:
+                    emb.set_image(url = image)
+                if thumbnail:
+                    emb.set_thumbnail(url = thumbnail)
+                if 't&' not in msg and 'd&' not in msg and 'img&' not in msg and 'th&' not in msg and 'c&' not in msg:
+                    if message.author == self.client.user:
+                        if '--clean' in msg:
+                            await message.edit(content = None)
+                        if '--delete' in msg:
+                            await message.delete()
+                        if '--noembed' in msg:
+                            if message.embeds != []:
+                                await message.edit(embed = None)
+                            else:
+                                return await ctx.send(f'{ctx.author.mention}, нечего удалять. Возможно, вы имели ввиду cy/edit {message.id} --delete ?', delete_after = 8)
+                        if '--empty-embed' in msg:
+                            if message.embeds != []:
+                                emb = discord.Embed(title = None, description = None, color = ctx.author.color)
+                                emb.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
+                                await message.edit(embed = emb)
+                            else:
+                                return await ctx.send(f'{ctx.author.mention}, нечего очищать. Возможно, вы имели ввиду cy/edit {message.id} --delete ?')
+                        else:
+                            await message.edit(content = msg)
+                    else:
+                        return await ctx.send(f'{message.id} не является сообщением от {self.client.user}')
+                else:
+                    if message.author == self.client.user:
+                        if '--clean' in msg:
+                            await message.edit(content = None, embed = emb)
+                        if '--noembed' in msg:
+                            if message.embeds != []:
+                                await message.edit(embed = None)
+                            else:
+                                return await ctx.send(f'{ctx.author.mention}, нечего удалять. Возможно, вы имели ввиду cy/edit {message.id} --delete ?', delete_after = 8)
+                        else:
+                            await message.edit(embed = emb)
+                    else:
+                        return await ctx.send(f'{message.id} не является сообщением от {self.client.user}')
         else:
-            c = int('0x' + c, 16)
-        if a == None:
-            a = ctx.author
-        if img == None:
-            img = ('')
-        if f == None:
-            f = ('')
-        if fu == None:
-            fu = ('Cephalon Cy by сасиска#2472')
-        emb = discord.Embed(title = t, description = d, colour = c)
-        emb.set_author(name = a, icon_url = a.avatar_url)
-        emb.set_image(url = img)
-        emb.set_thumbnail(url = f)
-        emb.set_footer(text = fu)
-        await message.edit(embed = emb)
-        await ctx.send('👌', delete_after = 1)
-    
-    @commands.command(aliases = ['Edit', 'EDIT'])
-    @commands.has_permissions(manage_channels = True)
-    async def edit(self, ctx, arg, *, text):
-        await ctx.message.delete()
-        message = await ctx.fetch_message(id = arg)
-        await message.edit(content = text)
-        await ctx.send('👌', delete_after = 1)
+            await ctx.send(f'сообщение {message.id} не обнаружено.')
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            await ctx.message.delete()
-            emb = discord.Embed(description = f'{ctx.author.mention}, команда в кд, потерпи чутка!', colour = discord.Color.orange())
-            emb.set_footer(text = 'По причине того, что этот еблан сломался - бесплатно раздаётся про версия. Для этого, напишите в лс сасиска#2472 с ID вашего сервера, далее я скину вам ссылку приглашение.')
-            await ctx.send(embed = emb)
+    @commands.command(aliases = ['ctx'])
+    async def content(self, ctx, arg, channel: discord.TextChannel = None):
+        if channel == None:
+            channel = ctx.message.channel
+        message = await channel.fetch_message(id = arg)
+        for emb in message.embeds:
+            if emb.color != emb.Empty:
+                color = f' color {emb.color}'
+            else:
+                color = ''
+            if emb.author.name != emb.Empty:
+                author = f' author {emb.author.name}'
+            else:
+                author = ''
+            if message.content == '':
+                content = ''
+            else:
+                content = f'content {message.content}'
+            if emb.image.url != emb.Empty:
+                img = f' | img& {emb.image.url}'
+                image = f' image {emb.image.url}'
+            else:
+                img = ''
+                image = ''
+            if emb.thumbnail.url != emb.Empty:
+                th = f' | th& {emb.thumbnail.url}'
+                thumb = f' thumbnail {emb.thumbnail.url}'
+            else:
+                th = ''
+                thumb = ''
+            if emb.description != emb.Empty:
+                d = f' | d& {emb.description}'
+                description = f' description {emb.description}'
+            else:
+                d = ''
+                description = ''
+            if emb.title != emb.Empty:
+                t = f't& {emb.title}'
+                title = f' title {emb.title}'
+            else:
+                t = ''
+                title = ''
+            if emb.footer.text != emb.Empty:
+                f = f' | f& {emb.footer.text}'
+                footer = f' footer {emb.footer.text}'
+            else:
+                f = ''
+                footer = ''
+        if message.author.id in botversions:
+            if message.embeds == []:
+                await ctx.send(f'```cy/say {message.content}```')
+            else:
+                await ctx.send(f'```cy/say {t}{d}{f}{th}{img}```')
+        else:
+            if message.embeds == []:
+                if '```' in message.content:
+                    await ctx.send(f'@{message.author} {message.content}')
+                else:
+                    await ctx.send(f'```@{message.author} {message.content}```')
+            else:
+                await ctx.send(f'```{content}{title}{description}{footer}{color}{author}{image}{thumb}```')
 
 def setup(client):
     client.add_cog(Embeds(client))
