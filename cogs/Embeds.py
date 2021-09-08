83202295c98972838c4da5879d06f68963da205b
D-9341 <58139230+D-9341@users.noreply.github.com>
2021-09-08 20:04:45 +0300

    Убрал футер с моим упоминанием

--- cogs/Embeds.py
+++ cogs/Embeds.py
@@ -47,8 +47,6 @@ class Embeds(commands.Cog):
                 emb.set_thumbnail(url = thumbnail)
             if footer:
                 emb.set_footer(text = footer)
-            if ctx.guild.owner.id != self.client.owner_id and ctx.guild.owner.id not in friends:
-                emb.set_footer(text = 'Cephalon Cy by сасиска#2472')
             if 't&' not in msg and 'd&' not in msg and 'img&' not in msg and 'th&' not in msg and 'msg&' not in msg and 'f&' not in msg and 'a&' not in msg:
                 return await ctx.send(msg)
             else:
