65fc66f1ae3526f3d73e87ceea320121641d1c85
D-9341 <58139230+D-9341@users.noreply.github.com>
2021-12-26 20:03:37 +0300

    Help
    Теперь вроде точно

--- cogs/sCephalon.py
+++ cogs/sCephalon.py
@@ -91,7 +91,7 @@ class sCephalon(commands.Cog):
     async def _help(self, inter, arg: str = commands.Param(default = None, choices = {'about': 'about', 'avatar': 'avatar', 'ban': 'ban', 'content': 'content', 'clear': 'clear', 'dm': 'dm', 'say': 'say', 'edit': 'edit', 'give': 'give', 'kick': 'kick', 'mute': 'mute', 'role': 'role', 'take': 'take', 'unmute': 'unmute', 'embeds': 'embeds', 'cephalon': 'cephalon', 'mod': 'mod', 'misc': 'misc', 'all': 'all', 'roll': 'roll'})):
         if arg == None:
             emb = disnake.Embed(title = self.client.user.name, description = 'Доступные Slash-команды', colour = disnake.Color.orange())
-            emb.add_field(name = 'Cephalon', value = '`info`, `invite`, `join`, `leave`, `ping`', inline = False)
+            emb.add_field(name = 'Cephalon', value = '`botver`, `info`, `invite`, `join`, `leave`, `ping`', inline = False)
             emb.add_field(name = 'Embeds', value = '`content`, `edit`, `say`', inline = False)
             if not (inter.guild.owner.id != self.client.owner_id and inter.guild.owner.id not in friends):
                 emb.add_field(name = 'Fun', value = '`aye_balbec`, `dotersbrain`, `niggers`, `rp`, `rap`, `zatka`', inline = False)
