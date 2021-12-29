86d0fbdf35316fc4091fffe53b7076521e7cc351
D-9341 <58139230+D-9341@users.noreply.github.com>
2021-12-29 23:26:10 +0300

    Update code.py

--- code.py
+++ code.py
@@ -245,7 +245,7 @@ async def on_message_edit(before, after):
 @client.event
 async def on_slash_command_error(inter, error):
     channel = client.get_channel(838506478108803112)
-    elif isinstance(error, commands.MissingPermissions):
+    if isinstance(error, commands.MissingPermissions):
         emb = disnake.Embed(description = f'{inter.author.mention}, у вас недостаточно прав на выполнение команды `{inter.command.name}`', colour = disnake.Color.orange())
         await inter.response.send_message(embed = emb)
         eemb = disnake.Embed(description = 'Поймана ошибка `MissingPermissions`', color = 0xff0000, timestamp = disnake.utils.utcnow())
